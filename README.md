3d299a3ec4d1b0453265a54f30184c045b31e4b9
theAgwoo <117565905+theAgwoo@users.noreply.github.com>
2022-11-06 09:11:12 +0000

    the first tutorials added

---
+++ README.md
@@ -0,0 +1,5 @@
+ - 👋welcome to my js reposotory!
+ - 👍this is where i can show you some code!
+ - in the src 📂folder there are 📁files of code you can look at!
+ - i would recomend installing node.js so you can run the code on your computer
+   and not on a web page, just because it is more efficient
---
+++ src/1 - basics.js
@@ -0,0 +1,18 @@
+// variables
+var name = "agwoo";
+var age  =  42;
+
+var message = "Hi, my name is " + name + " abd I am " + age + " years old!";
+
+console.log(message);
+
+
+//!    how to run 
+//!     this code
+
+
+// cd into dir
+// run node basics.js
+
+//* or you can make a .html file and link to it and open 
+//* it and open the dev console
